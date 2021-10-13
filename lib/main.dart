c238fbb57d6177c5a09fe4bf03b839d4a8d56269
khaledalaa720 <69543709+khaledalaa720@users.noreply.github.com>
2021-10-13 14:11:37 +0200

    SplashScren

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -28,21 +33,35 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Elm2oul"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+      // appBar: AppBar(
+      //   title: Text("Elm2oul"),
+      // ),
+      body: SplashScreenView(
+        navigateRoute: MyApp(),
+        duration: 3000,
+        imageSize: 250,
+        imageSrc: "lib/Images/splashImage.png",
+        text: "مقاول",
+        textType: TextType.NormalText,
+        textStyle: TextStyle(
+          fontSize: 70,
+          foreground: Paint()..shader = linearGradient,
+          fontWeight: FontWeight.w400,
         ),
       ),
     );
   }
+  final Shader linearGradient = LinearGradient(
+    colors: <Color> [
+      HexColor("#FA7E75100"),
+      HexColor("#EABEC9"),
+      HexColor("#F399A2"),
+      HexColor("#FF903C"),
+      HexColor("#FB767D"),
+      HexColor("#FB767D"),
+      HexColor("#FFAF35"),
+      HexColor("#F88C68"),
+    ],
+  ).createShader(Rect.fromLTWH(200.0,70.0, 200.0,1.0));
 }
